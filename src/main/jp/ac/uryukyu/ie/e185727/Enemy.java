39fc92da517759036d5f9c72d468a6d2d26ce105
Yumi Omoto <e185727@ie.u-ryukyu.ac.jp>
2019-01-21 01:50:23 +0900

    2nd commit

--- src/main/jp/ac/uryukyu/ie/e185727/Enemy.java
+++ src/main/jp/ac/uryukyu/ie/e185727/Enemy.java
@@ -46,9 +46,11 @@ public class Enemy {
      * @param hero 攻撃対象
      */
     public void attack(Hero hero){
+        if(dead == false) {
         int damage = (int)(Math.random() * attack);
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
-        hero.wounded(damage);
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
+        }
+
     }
 
     /**
